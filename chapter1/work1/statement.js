be79efae40f87f5fe9039bcdeba3f63d2d0c8f9a
ygotoiY <ygoto@iyell.jp>
2023-04-20 18:56:43 +0900

    メソッドの抽出

--- chapter1/work1/statement.js
+++ chapter1/work1/statement.js
@@ -7,33 +7,37 @@ function statement (invoice, plays) {
     const format = new Intl.NumberFormat("en-US",
                           { style: "currency", currency: "USD",
                             minimumFractionDigits: 2 }).format;
-  
+
+    function amountFor(play, perf) {
+        let result = 0;
+        switch (play.type) {
+            case "tragedy":
+                result = 40000;
+                if (perf.audience > 30) {
+                    result += 1000 * (perf.audience - 30);
+                }
+                break;
+            case "comedy":
+                result = 30000;
+                if (perf.audience > 20) {
+                    result += 10000 + 500 * (perf.audience - 20);
+                }
+                result += 300 * perf.audience;
+                break;
+            default:
+                throw new Error(`unknown type: ${play.type}`);
+        }
+        return result;
+    }
+
     for (let perf of invoice.performances) {
       // 2. 問い合わせによる一時変数の置き換え
       // 3. 関数宣言の変更
       const play = plays[perf.playID];
 
-      let thisAmount = 0;
-      // 1. 関数の抽出を行う
-      switch (play.type) {
-      case "tragedy":
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case "comedy":
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-          throw new Error(`unknown type: ${play.type}`);
-      }
+      let thisAmount = amountFor(play, perf);
 
-      // add volume credits
+        // add volume credits
       volumeCredits += Math.max(perf.audience - 30, 0);
       // add extra credit for every ten comedy attendees
       if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
